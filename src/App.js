ad94558cc1c444f4af108c780901644ba838ee3a
Muhammad Tanveer Sultan <tanveer6959612@gmail.com>
2019-10-27 09:07:19 -0700

    this is first ReactApp

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const abc=()=>{
+    alert("okey Google ")
+  }
+  return (
+      <body>
+      <input type="button" value="btn" onClick={abc}/>
+
+
+      </body>
+  );
+}
+
+export default App;
